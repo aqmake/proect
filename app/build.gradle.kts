688ef10125b5f55fd051cd1a9f569ada3086cfbd
aqmake <artemradi.2002@gmail.com>
2025-02-21 12:00:04 +0700

    Added checkbox and text

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.tasks"
-        minSdk = 24
+        minSdk = 30
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
--- app/src/main/java/com/example/tasks/ui/Screen/Sign up.kt
+++ app/src/main/java/com/example/tasks/ui/Screen/Sign up.kt
@@ -1,7 +1,10 @@
 package com.example.tasks.ui.Screen
 
+import android.util.Log
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -12,6 +15,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Checkbox
@@ -27,17 +31,23 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.tasks.R
+import com.example.tasks.ui.theme.Blue80
 
 
 fun Color.Companion.gold() = Color(0xFFFFFFFF)
@@ -52,6 +62,7 @@ fun SingUp(
     var text3 by remember { mutableStateOf("") }
     var text4 by remember { mutableStateOf("") }
     var text5 by remember { mutableStateOf("") }
+    var check by remember { mutableStateOf(false) }
 
 
     Column(
@@ -92,7 +103,9 @@ fun SingUp(
                 )
             },
             shape = RoundedCornerShape(4.dp),
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(5.dp)
 
         )
 
@@ -216,8 +229,64 @@ fun SingUp(
             shape = RoundedCornerShape(4.dp),
         )
         Row() {
+            Checkbox(checked = check,
+                onCheckedChange = {
+                    check = it
+                })
+            Spacer(modifier = Modifier.size(11.dp))
+            val annotatedText = buildAnnotatedString {
+                withStyle(
+                    SpanStyle(
+                        fontWeight = FontWeight.W400,
+                        fontSize = 12.sp,
+                        color = Color(0xffa6a6a6)
+                    )
+                ) {
+                    append("By ticking this box, you agree to our")
+                }
+                pushStringAnnotation(
+                    tag = "SignUp",// provide tag which will then be provided when you click the text
+                    annotation = "SignUp45"
+                )
+                withStyle(
+                    SpanStyle(
+                        fontWeight = FontWeight.W400,
+                        fontSize = 12.sp,
+                        color = Color(0xffeabc2e)
+                    )
+                ) {
+                    append("Terms and conditions and private policy")
+                }
+
+            }
+            ClickableText(
+                text = annotatedText,
+                onClick = { offset ->
+
+                    annotatedText.getStringAnnotations(
+                        tag = "SignUp",
+                        start = offset,
+                        end = offset
+                    )[0].let { annotation ->
+
+
+                        Log.w("Clicked", annotation.item)
+
+
+                    }
+
+                }
+            )
 
         }
+        Spacer(modifier = Modifier.size(64.dp))
+        Box(modifier = Modifier
+            .size(342.dp, 46.dp)
+            .background(color = Blue80, shape = RoundedCornerShape(5.dp))
+            .clip(shape = RoundedCornerShape(4.dp))
+            .clickable {
+                onClick()
+            })
     }
 }
 
